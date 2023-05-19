037332c3513f372212bab7be14bec3b37ae8aa5b
Roman Sobkuliak <r.sobkuliak@gmail.com>
2023-05-19 12:25:46 +0200

    Add static stack definitions for Zephyr

--- compiler/lib/src/main/scala/codegen/CppWriter/TopologyCppWriter/TopComponentInstances.scala
+++ compiler/lib/src/main/scala/codegen/CppWriter/TopologyCppWriter/TopComponentInstances.scala
@@ -39,13 +39,35 @@ case class TopComponentInstances(
     def getCode(ci: ComponentInstance): List[Line] = {
       val implType = getImplType(ci)
       val instanceName = getNameAsIdent(ci.qualifiedName)
-      getCodeLinesForPhase (CppWriter.Phases.instances) (ci).getOrElse(
-        lines(
+
+      getCodeLinesForPhase (CppWriter.Phases.instances) (ci).getOrElse{
+        def getStackDefCode(ci: ComponentInstance): List[Line] = {
+          val stackSize = ci.stackSize.getOrElse(Nil)
+          List(
+            line("#ifdef TGT_OS_TYPE_ZEPHYR"),
+            line(s"K_THREAD_STACK_DEFINE(${instanceName}_stack, $stackSize);"),
+            line(s"NATIVE_UINT_TYPE ${instanceName}_stack_size_real = K_THREAD_STACK_SIZEOF(${instanceName}_stack);"),
+            line("#endif")
+          )
+        }  
+
+        val result = lines(
           s"$implType $instanceName(FW_OPTIONAL_NAME($q$instanceName$q));"
         )
-      )
+
+        if (isActive(ci)) {
+          result ::: getStackDefCode(ci)
+        } else {
+          result
+        }
+      }
     }
-    flattenWithBlankPrefix(instances.map(getCode))
+    flattenWithBlankPrefix(
+      List(List(
+        line(s"#ifdef TGT_OS_TYPE_ZEPHYR"),
+        line(s"#include <zephyr/kernel.h>"),
+        line("#endif")
+      )) ++ instances.map(getCode))
   }
 
   private def getImplType(ci: ComponentInstance) = {
